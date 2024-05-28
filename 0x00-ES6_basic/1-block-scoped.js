f997ea7cc5774031e395121220484462c49632d4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-05-28 02:19:39 +0200

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// 1-block-scoped.js
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
+
