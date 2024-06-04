47da45832861779fab22e9e8c519036cbd1ec8f6
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-04 22:18:01 +0100

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents() {
+	const students = [
+		{ id: 1, firstname: 'Guillaume', location: 'San Francisco'},
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Francisco' }
+	];
+	return students;
+}
+export default getListStudents;
+	
