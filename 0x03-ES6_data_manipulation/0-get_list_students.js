6697f2a25424a8f738e9e518e9846ebb14e7babc
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-14 19:26:57 +0100

    get_list_students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds() {
+  return ([
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ]);
+}
