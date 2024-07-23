88ec2c464bf0a94e0ee30ed53be10a6fa1b382c5
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-23 16:51:57 +0100

    feat: Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-const displayMessage(message) {
-    console.log("message");
+function displayMessage(message) {
+    console.log(message);
 }
 
-module.exports = displayMessage; 
+module.exports = displayMessage;
