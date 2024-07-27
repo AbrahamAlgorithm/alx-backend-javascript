eefb1bcfc81e0cc73f3ee08cf5d3644a97b166ca
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-27 08:33:12 +0100

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,71 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  const url = 'http://localhost:7865';
+
+  it('should return status code 200', (done) => {
+    request(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request(url, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return status code 200 when :id is a number', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('Available payments', () => {
+  it('should return the correct payment methods', (done) => {
+    request('http://localhost:7865/available_payments', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+      done();
+    });
+  });
+});
+
+describe('Login', () => {
+  it('should return the correct welcome message', (done) => {
+    const options = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: {
+        userName: 'Betty'
+      }
+    };
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
+
